53810e541ec76012943b9a186e21df166b4ed131
BenjaminHavery <benjamin.havery@gmail.com>
2021-05-16 23:34:30 +0100

    Add UI, messages, colors

--- components/Wall.js
+++ components/Wall.js
@@ -2,15 +2,25 @@ import { useState, useEffect, useRef } from 'react'
 
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three';
+import { Html, Text } from '@react-three/drei'
 
 // Smoothing function
 const lerp = (a, b, t) => ((1 - t) * a + t * b)
 
-// Brick proportions
-const dim = { x: 4, y: 1, z: 2 };
+
+// const messages = [
+//   // {
+//   //   text: 'a message',
+
+//   //   y: (Math.random() - 0.5) * count.y * dim.y/2
+//   // }
+//   // ,
+//   'another message',
+//   'huh?',
+// ];
 
 
-export default function Wall({ count, prog, ...props }) {
+export default function Wall({ dim, count, prog, messages, ...props }) {
   
   const scroller = useRef();
   
@@ -19,14 +29,46 @@ export default function Wall({ count, prog, ...props }) {
   return (
     <group {...props} ref={scroller}>
 
-      <Bricks key={`boxes--${count.x}-${count.y}`} {...{ count }}/>
+      <Bricks key={`boxes--${count.x}-${count.y}`} {...{ dim, count }}/>
+      { messages.map((message) => 
+        <Html
+          key={message.id}
+          position={[message.x, message.y, dim.z/2]}
+          transform={true}
+          zIndexRange={[1000, 0]}
+          style={{
+            userSelect: 'none',
+            pointerEvents: 'none',
+          }}
+        >
+          <div style={{
+            width: '400px',
+            color: message.color,
+            fontSize: '80px',
+            lineHeight: 1,
+            fontFamily: 'ADrip',
+            textAlign: 'center',
+            fontStyle: 'italic',
+          }}>
+            <span>{ message.text }</span>
+          </div>
+        </Html>
+        // <Text
+        //   // key={index}
+        //   // color="white" // default
+        //   // anchorX="center" // default
+        //   // anchorY="middle" // default
+        // >
+        //   { message }
+        // </Text>
+      )}
 
     </group>
   )
 }
 
 
-const Bricks = ({ count }) => {
+const Bricks = ({ dim, count }) => {
 
   const bricks = useRef(),
         mortar = useRef();
@@ -64,6 +106,8 @@ const Bricks = ({ count }) => {
           <meshStandardMaterial
             attach="material"
             color='#fc693d'
+            roughness={1}
+            metalness={0}
           />
       </instancedMesh>
       <instancedMesh ref={mortar} position={[0, -dim.y*count.y/2, 0]} args={[null, null, count.x*count.y]}>
@@ -71,6 +115,8 @@ const Bricks = ({ count }) => {
           <meshStandardMaterial
             attach="material"
             color='#c7c1bf'
+            roughness={1}
+            metalness={0}
           />
       </instancedMesh>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,89 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { nanoid } from 'nanoid'
+
+const useTimeout = (callback, delay) => {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setTimeout(tick, delay);
+      return () => clearTimeout(id);
+    }
+  }, [delay]);
+};
+
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
 
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+import { FaSprayCan, FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 
 import Wall from '../components/Wall'
 
-const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
+const colors = [
+  '#e8bd42',
+  '#40b3ed',
+  '#40ed88',
+  '#ff91eb',
+  '#f5f5f5',
+  '#546bff',
+  '#383838',
+];
 
 
 
 export default function WallPage() {
 
-  const [prog, setProg] = useState(0);
+  const [prog, setProg] = useState(0),
+        [newMessage, setNewMessage] = useState(''),
+        [messages, setMessages] = useState([]);
 
+
+  useTimeout(() => {
+    addMessage('Did you come here to WRITE on my WALL? :)');
+  }, 2000);
+  useTimeout(() => {
+    addMessage('DRAG to LOOK =========== ARROWS or SCROLL to MOVE');
+  }, 5000)
+
+  // Brick meta
+  const dim = { x: 4, y: 1, z: 2 };
   const count = {
-    x: 101,
+    x: 501,
     y: 20,
   };
 
-  const updateVirtualScroll = (e) => {
-    const newProg = clamp(prog + Math.floor(e.deltaY/100), 0, count.x - 2);
+  const addMessage = (text) => {
+    if (!text.length) return;
+    const message = {
+      id: nanoid(),
+      text: text,
+      x: 3 * (messages.length+0.5) * dim.x,
+      y: (Math.random() - 0.5) * count.y * dim.y/2,
+      color: colors[Math.floor(Math.random() * colors.length)],
+    }
+    setNewMessage('');
+    setMessages([...messages, message]);
+    setProg(message.x/dim.x);
+    console.log(messages, prog);
+  }
+  
+  const updateVirtualScroll = (change) => {
+    const newProg = clamp(prog + Math.floor(change), 0, count.x - 2);
     if (prog !== newProg) setProg(newProg);
   }
 
   return (
-    <div className='wrapper' onWheel={(e) => updateVirtualScroll(e)}>
+    <div className='wrapper' onWheel={(e) => updateVirtualScroll(e.deltaY/100)}>
       <Canvas className='canvas' camera={{ position: [0, 0, 15]}}>
         
         <ambientLight intensity={0.1} />
@@ -32,7 +91,10 @@ export default function WallPage() {
         <axesHelper args={[10, 10, 10]} />
 
         <fog attach="fog" args={['#000000', 10, 200]} />
-        <Wall {...{ count, prog }}/>
+        <Wall
+          messages={ messages.filter(m => prog*dim.x - 70 < m.x && prog*dim.x + 70 > m.x) }
+          {...{ dim, count, prog,  }}
+        />
 
         <OrbitControls
           maxPolarAngle={1.8}
@@ -44,10 +106,38 @@ export default function WallPage() {
         />
       </Canvas>
 
-      {/* <div className='controls'>
-        <button onClick={() => setProg(prog - 1)}>back</button>
-        <button onClick={() => setProg(prog + 1)}>forward</button>
-      </div> */}
+      <div className='controls'>
+        <div className='row'>
+          <button
+            className='button'
+            onClick={() => updateVirtualScroll(-2)}
+            >
+            <FaChevronLeft/>
+          </button>
+          <button
+            className='button'
+            onClick={() => updateVirtualScroll(2)}
+            >
+            <FaChevronRight/>
+          </button>
+        </div>
+        <div className='row'>
+          <input
+            className='input'
+            type="text"
+            value={newMessage}
+            placeholder='Type your message'
+            onChange={(e) => setNewMessage(e.target.value)}
+          />
+          <button
+            className='button'
+            onClick={() => addMessage(newMessage)}
+            >
+            <FaSprayCan/>
+          </button>
+        </div>
+        
+      </div>
       
       <style jsx>{`
         .wrapper {
@@ -62,9 +152,56 @@ export default function WallPage() {
 
         .controls {
           position: absolute;
+          display: flex;
+          flex-flow: row wrap;
+          align-items: flex-end;
+          z-index: 100000;
+          top: 0;
+          left: 0;
           bottom: 0;
           right: 0;
-          background: black;
+          pointer-events: none;
+        }
+
+        .row {
+          display: flex;
+          flex-flow: row nowrap;
+          align-items: flex-end;
+          justify-content: space-between;
+          width: 100%;
+          padding: 10px;
+        }
+
+        .input, .button {
+          pointer-events: all;
+        }
+
+        .input {
+          display: block;
+          flex: 1 1 auto;
+          margin: 0 15px 0 0;
+          padding: 10px 20px;
+          border: none;
+          border-radius: 15px;
+          outline: none;
+          background: white;
+          font-size: 16px;
+          text-align: right;
+        }
+
+        .button {
+          display: flex;
+          flex-flow: row nowrap;
+          align-items: flex-end;
+          flex: 0 0 auto;
+          margin: 0;
+          padding: 0;
+          color: white;
+          border: none;
+          outline: none;
+          background: none;
+          font-size: 56px;
+          cursor: pointer;
         }
       `}</style>
       
