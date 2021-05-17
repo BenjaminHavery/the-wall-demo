7a41f54a302dc6890fab79ba2200c10f95b388d8
BenjaminHavery <benjamin.havery@gmail.com>
2021-05-17 01:53:08 +0100

    Reduce scale, tune controls,

--- components/Wall.js
+++ components/Wall.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three';
@@ -6,18 +6,6 @@ import { Html, Text } from '@react-three/drei'
 
 // Smoothing function
 const lerp = (a, b, t) => ((1 - t) * a + t * b)
-
-
-// const messages = [
-//   // {
-//   //   text: 'a message',
-
-//   //   y: (Math.random() - 0.5) * count.y * dim.y/2
-//   // }
-//   // ,
-//   'another message',
-//   'huh?',
-// ];
 
 
 export default function Wall({ dim, count, prog, messages, ...props }) {
@@ -42,9 +30,9 @@ export default function Wall({ dim, count, prog, messages, ...props }) {
           }}
         >
           <div style={{
-            width: '400px',
+            width: '100px',
             color: message.color,
-            fontSize: '80px',
+            fontSize: '20px',
             lineHeight: 1,
             fontFamily: 'ADrip',
             textAlign: 'center',
@@ -79,19 +67,19 @@ const Bricks = ({ dim, count }) => {
     for (let ix = 0; ix < count.x; ++ix) {
       for (let iy = 0; iy < count.y; ++iy) {
         
-        const setAt = ix*count.y + iy,
-              offMult = iy%2;
+        const setAt = ix*count.y + iy, // which brick?
+              offMult = iy%2; // multiplier for adjusting odd rows
         
+        // Brick
         var x = dim.x * (ix - offMult*0.5),
-            y = dim.y * count.y - iy - 1;
-        
+            y = dim.y * (count.y - iy - 0.5);
         transform.setPosition(x, y, 0)
         bricks.current.setMatrixAt(setAt, transform)
         
-        if (iy == 0) y = y - 0.2;
-        if (ix == 0) x = x + 0.2 + offMult * dim.x/2;
-        if (ix == count.x - 1) x = x - 0.2 - (1 - offMult) * dim.x/2;
-        
+        // Mortar
+        if (iy == 0) y = y - 2*dim.sp; // clear top edge
+        if (ix == 0) x = x + 2*dim.sp + offMult*dim.x/2; // clear left edge
+        if (ix == count.x - 1) x = x - 2*dim.sp - (1 - offMult)*dim.x/2; // clear right edge
         transform.setPosition(x, y, 0)
         mortar.current.setMatrixAt(setAt, transform)
       }
@@ -102,7 +90,7 @@ const Bricks = ({ dim, count }) => {
   return (
     <>
       <instancedMesh ref={bricks} position={[0, -dim.y*count.y/2, 0]} args={[null, null, count.x*count.y]}>
-        <boxBufferGeometry args={[0.975*dim.x, 0.9*dim.y, dim.z]} />
+        <boxBufferGeometry args={[(1 - dim.sp/dim.x)*dim.x, (1 - dim.sp/dim.y)*dim.y, dim.z]} />
           <meshStandardMaterial
             attach="material"
             color='#fc693d'
@@ -111,7 +99,7 @@ const Bricks = ({ dim, count }) => {
           />
       </instancedMesh>
       <instancedMesh ref={mortar} position={[0, -dim.y*count.y/2, 0]} args={[null, null, count.x*count.y]}>
-        <boxBufferGeometry args={[1.025*dim.x, 1.1*dim.y, 0.9*dim.z]} />
+        <boxBufferGeometry args={[(1 + dim.sp/dim.x)*dim.x, (1 + dim.sp/dim.y)*dim.y, (1 - dim.sp/dim.z)*dim.z]} />
           <meshStandardMaterial
             attach="material"
             color='#c7c1bf'
--- pages/index.js
+++ pages/index.js
@@ -40,6 +40,13 @@ const colors = [
 ];
 
 
+// Brick meta
+const dim = { x: 1, y: 0.25, z: 0.5, sp: 0.025, ms: 3 };
+const count = {
+  x: 501,
+  y: 20,
+};
+  
 
 export default function WallPage() {
 
@@ -47,33 +54,30 @@ export default function WallPage() {
         [newMessage, setNewMessage] = useState(''),
         [messages, setMessages] = useState([]);
 
-
   useTimeout(() => {
-    addMessage('Did you come here to WRITE on my WALL? :)');
-  }, 2000);
+    addMessage('A Ben H demo ~-~<>~-~ benhavery.info ~-~-~-~');
+  }, 0)
   useTimeout(() => {
     addMessage('DRAG to LOOK =========== ARROWS or SCROLL to MOVE');
-  }, 5000)
+  }, 2500)
+  useTimeout(() => {
+    addMessage('Would YOU like to WRITE on my WALL?');
+  }, 5000);
 
-  // Brick meta
-  const dim = { x: 4, y: 1, z: 2 };
-  const count = {
-    x: 501,
-    y: 20,
-  };
 
   const addMessage = (text) => {
     if (!text.length) return;
+    const prevX = !!messages.length ? messages[messages.length-1].x : 0;
     const message = {
       id: nanoid(),
       text: text,
-      x: 3 * (messages.length+0.5) * dim.x,
+      x: prevX + dim.ms,
       y: (Math.random() - 0.5) * count.y * dim.y/3,
       color: colors[Math.floor(Math.random() * colors.length)],
     }
     setNewMessage('');
     setMessages([...messages, message]);
-    setProg(message.x/dim.x);
+    setProg(message.x);
     console.log(messages, prog);
   }
   
@@ -83,16 +87,16 @@ export default function WallPage() {
   }
 
   return (
-    <div className='wrapper' onWheel={(e) => updateVirtualScroll(e.deltaY/100)}>
-      <Canvas className='canvas' camera={{ position: [0, 0, 15]}}>
+    <div className='wrapper' onWheel={(e) => updateVirtualScroll(Math.sign(e.deltaY))}>
+      <Canvas className='canvas' camera={{ position: [0, 0, count.y*dim.y/1.4]}}>
         
         <ambientLight intensity={0.1} />
         <pointLight intensity={0.8} position={[10, 10, 10]} />
         {/* <axesHelper args={[10, 10, 10]} /> */}
 
-        <fog attach="fog" args={['#000000', 10, 200]} />
+        <fog attach="fog" args={['#000000', 1*dim.ms, 10*dim.ms]} />
         <Wall
-          messages={ messages.filter(m => prog*dim.x - 70 < m.x && prog*dim.x + 70 > m.x) }
+          messages={ messages.filter(m => prog - 6*dim.ms < m.x && prog + 6*dim.ms > m.x) }
           {...{ dim, count, prog,  }}
         />
 
@@ -110,13 +114,13 @@ export default function WallPage() {
         <div className='row'>
           <button
             className='button'
-            onClick={() => updateVirtualScroll(-2)}
+            onClick={() => updateVirtualScroll(-dim.ms)}
             >
             <FaChevronLeft/>
           </button>
           <button
             className='button'
-            onClick={() => updateVirtualScroll(2)}
+            onClick={() => updateVirtualScroll(dim.ms)}
             >
             <FaChevronRight/>
           </button>
@@ -160,7 +164,7 @@ export default function WallPage() {
           left: 0;
           bottom: 0;
           right: 0;
-          padding-bottom: 50px;
+          padding-bottom: 60px;
           pointer-events: none;
         }
 
@@ -175,6 +179,10 @@ export default function WallPage() {
 
         .input, .button {
           pointer-events: all;
+          opacity: 0.5;
+        }
+        .input:focus {
+          opacity: 1;
         }
 
         .input {
@@ -205,9 +213,17 @@ export default function WallPage() {
           cursor: pointer;
         }
 
+        @media screen and (max-width: 1000px) {
+          .button:focus {
+            opacity: 1;
+          }
+        }
         @media screen and (min-width: 1000px) {
           .controls {
             padding-bottom: 0;
+          }
+          .input:hover, .button:hover {
+            opacity: 1;
           }
         }
       `}</style>
